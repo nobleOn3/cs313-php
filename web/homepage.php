a63a06d49c6f77e780fb12deefdf18c4b34a1b11
nobleOn3 <nob16001@byui.edu>
2019-01-16 10:51:48 -0700

    Added stylesheet and base homepage file

---
+++ web/homepage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8"/>
+   <meta name="viewport" content="width=device-width, initial-scale=1"/>
+   <title>J.Noble's Homepage</title>
+   <style>
+   table, th, td{
+   	   border: 1px solid black;
+   }
+
+   #hello {
+   	color: blue;
+   	font-size: 75px;
+   }
+   
+   </style>
+</head>
+<body style="background-color: grey;">
+   <div style="text-align: center; margin: auto; width: 800px;">
+      <h1 style="color: royalblue; font-family:courier;"><i>Noble's Homepage</i></h1>
+      <input type="button" onclick="document.getElementById('hello').innerHTML = 'Jessen is king!'" value="Click Here!">
+      <h1 id="hello"></h1>
+   </div>
+ 
+</body>
+</html>
