64ae881d5067e0f5f50e973182e155d092ed42df
nobleOn3 <nob16001@byui.edu>
2019-01-16 12:05:00 -0700

    Changed the page format yet again

--- web/homepage.php
+++ web/homepage.php
@@ -9,11 +9,11 @@
 <body style="background-color: grey;">
    <div class="center">
       <h1 class="title"><i>Noble's Homepage</i></h1>
-      <p class="intro">Hi, my name is Jessen Noble. Im a <b>software engineering major</b> at BYU-Idaho. I claim both Seattle, WA <b>and</b> Burley, ID as hometowns. </p>
       <?php 
-         echo "<p class='intro'>Today is " . date("m/d/Y") . "</p><br/>";
+         echo "<p class='intro'>Today is " . date("m/d/Y") . "</p>";
          echo "<p class='intro'>Today is " . date("l") . "</p><br/>";
       ?>
+      <p class="intro">Hi, my name is Jessen Noble. Im a <b>software engineering major</b> at BYU-Idaho. I claim both Seattle, WA <b>and</b> Burley, ID as hometowns. </p>
       <input type="button" onclick="document.getElementById('hello').innerHTML = 'Jessen is king!'" value="Click Here!">
       <h1 id="hello"></h1>
    </div>
