90c0d6c54cb7baa3a301f188ef46bc07deaa2325
nobleOn3 <nob16001@byui.edu>
2019-01-16 11:48:51 -0700

    Added date display to the page

--- web/homepage.php
+++ web/homepage.php
@@ -10,6 +10,9 @@
    <div class="center">
       <h1 class="title"><i>Noble's Homepage</i></h1>
       <p class="intro">Hi, my name is Jessen Noble. Im a <b>software engineering major</b> at BYU-Idaho. I claim both Seattle, WA <b>and</b> Burley, ID as hometowns. </p>
+      <?php 
+         echo "Today is " . date("Y/m/d") . "<br/>";
+      ?>
       <input type="button" onclick="document.getElementById('hello').innerHTML = 'Jessen is king!'" value="Click Here!">
       <h1 id="hello"></h1>
    </div>
