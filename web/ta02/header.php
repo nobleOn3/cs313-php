1fab6b9917c223f3fef5d35938553efdf822722c
nobleOn3 <nob16001@byui.edu>
2019-01-17 16:18:14 -0700

    Added highlighted current webpage functionality to navbar

--- web/ta02/header.php
+++ web/ta02/header.php
@@ -1,9 +1,12 @@
 <h1>Totally Not Nike's</h1>
+<?php
+   $page = "__FILE__";
+   }
 <nav>
     <ul>
-	   <li class="home"><a href="home.php">Home</a></li>
-	    <li class="about"><a href="aboutus.php">About Us</a></li>
-       <li class="login"><a href="login.php">Login</a></li>
+	   <li class="home <?php if ($page == 'home.php') echo 'active'; ?>"><a href="home.php">Home</a></li>
+	    <li class="about <?php if ($page == 'aboutus.php') echo 'active'; ?>"><a href="aboutus.php">About Us</a></li>
+       <li class="login <?php if ($page == 'login.php') echo 'active'; ?>"><a href="login.php">Login</a></li>
     </ul>
 </nav>
 
