ed03a8dda93ff4d5a6da1b7dbe3d1bfdf4c18fe0
nobleOn3 <nob16001@byui.edu>
2019-01-17 11:20:21 -0700

    still trying to fix home page

--- web/ta02/header.php
+++ web/ta02/header.php
@@ -1,7 +1,8 @@
-<ul>
-	<!--<li <?php if ($currentPage == 'Home') {echo 'class' ="active";} ?><a href="">Home</a></li>-->
-	<li class="home"><a href="home.php">Home</a></li>
-	<li class="about"><a href="aboutus.php">About Us</a></li>
-	<li class="login"><a href="login.php">Login</a></li>
-</ul>
+<?php
+    echo <ul>;
+	echo <li class="home"><a href="home.php">Home</a></li>;
+	echo <li class="about"><a href="aboutus.php">About Us</a></li>;
+	echo <li class="login"><a href="login.php">Login</a></li>;
+    echo </ul>;
+?>
 
