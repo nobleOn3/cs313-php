b8e59c607cb0edc3b9bc09ade33d8496f27d7629
nobleOn3 <nob16001@byui.edu>
2019-01-17 15:27:48 -0700

    still trying to fix home page

--- web/ta02/header.php
+++ web/ta02/header.php
@@ -1,8 +1,9 @@
-<?php
-    echo <ul>;
-	echo <li class="home"><a href="home.php">Home</a></li>;
-	echo <li class="about"><a href="aboutus.php">About Us</a></li>;
-	echo <li class="login"><a href="login.php">Login</a></li>;
-    echo </ul>;
-?>
+<nav>
+    <ul>;
+	   <li class="home"><a href="home.php">Home</a></li>
+	    <li class="about"><a href="aboutus.php">About Us</a></li>
+       <li class="login"><a href="login.php">Login</a></li>
+    </ul>
+</nav>
+
 
