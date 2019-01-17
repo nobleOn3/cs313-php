3a13b828c0741b93a5d250427bd64ab3b33d5cdc
nobleOn3 <nob16001@byui.edu>
2019-01-17 15:55:51 -0700

    Added Company Name to the header

--- web/ta02/header.php
+++ web/ta02/header.php
@@ -1,5 +1,6 @@
+<h1>Totally Not Nike's</h1>
 <nav>
-    <ul>;
+    <ul>
 	   <li class="home"><a href="home.php">Home</a></li>
 	    <li class="about"><a href="aboutus.php">About Us</a></li>
        <li class="login"><a href="login.php">Login</a></li>
