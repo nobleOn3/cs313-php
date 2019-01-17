92405dd505c8471160df594dfe2fcb4b6146f116
nobleOn3 <nob16001@byui.edu>
2019-01-17 11:04:40 -0700

    adding header to home page

--- web/ta02/home.php
+++ web/ta02/home.php
@@ -7,6 +7,7 @@
    <link rel="stylesheet" type="text/css" href="">
 </head>
 <body style="background-color: grey;">
+   <?php include('header.php'); ?>
    <div>
       <?php 
          $welvar = NULL;
