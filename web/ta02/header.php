3d51e594070aa0258c93c90de25580a8ba2cb1c7
nobleOn3 <nob16001@byui.edu>
2019-01-17 11:18:12 -0700

    still trying to fix home page

--- web/ta02/header.php
+++ web/ta02/header.php
@@ -1,32 +1,7 @@
-<!DOCTYPE html>
-<html>
-<header>
-	<?php
-	   $page = NULL;
+<ul>
+	<!--<li <?php if ($currentPage == 'Home') {echo 'class' ="active";} ?><a href="">Home</a></li>-->
+	<li class="home"><a href="home.php">Home</a></li>
+	<li class="about"><a href="aboutus.php">About Us</a></li>
+	<li class="login"><a href="login.php">Login</a></li>
+</ul>
 
-	   function makeClassActive() {
-
-	      if(page == "home.php") {
-             
-	      }
-
-	      else if ($page == "login.php") {
-
-	      }
-
-	      else if ($page == "aboutus.php") {
-
-	      }
-	  }
-	?>
-</header> 
-
-<body>
-	<ul>
-		<!--<li <?php if ($currentPage == 'Home') {echo 'class' ="active";} ?><a href="">Home</a></li>-->
-		<li class="home"><a href="home.php">Home</a></li>
-		<li class="about"><a href="aboutus.php">About Us</a></li>
-		<li class="login"><a href="login.php">Login</a></li>
-	</ul>
-</body>
-</html>
