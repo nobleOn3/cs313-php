4e3183e033009e24b6e8aa9943f475a5ed7ed3fc
nobleOn3 <nob16001@byui.edu>
2019-01-17 15:41:30 -0700

    added the navbar to all pages

--- web/ta02/aboutus.php
+++ web/ta02/aboutus.php
@@ -5,8 +5,33 @@
    <meta name="viewport" content="width=device-width, initial-scale=1"/>
    <title>About Totally not Nike's</title>
    <link rel="stylesheet" type="text/css" href="">
+   <style>
+   ul {
+   	list-style-type: none;
+   	margin: 0;
+   	padding: 5px;
+   }
+
+   li {
+   	float: left;
+   }
+
+   li a.active {
+   	 background-color: #4CAF50;
+   	 color: white;
+   }
+
+   li a:hover:not(.active) {
+   	background-color: #555;
+   	color:white;
+   }
+
+   </style>
 </head>
 <body style="background-color: grey;">
+	<?php 
+      include "./header.php"; 
+    ?>
    <div>
       <h1><i>About us</i></h1>
       <p>While we are not Nike, we love them and we try to follow their every move in our business model. Please check out our fine Totally Not Nike's Products.
--- web/ta02/login.php
+++ web/ta02/login.php
@@ -5,8 +5,34 @@
    <meta name="viewport" content="width=device-width, initial-scale=1"/>
    <title>Totally not Nike's Login</title>
    <link rel="stylesheet" type="text/css" href="">
+
+   <style>
+   ul {
+   	list-style-type: none;
+   	margin: 0;
+   	padding: 5px;
+   }
+
+   li {
+   	float: left;
+   }
+
+   li a.active {
+   	 background-color: #4CAF50;
+   	 color: white;
+   }
+
+   li a:hover:not(.active) {
+   	background-color: #555;
+   	color:white;
+   }
+
+   </style>
 </head>
 <body style="background-color: grey;">
+   <?php 
+      include "./header.php"; 
+   ?>
    <div>
       <h1><i>Login</i></h1>
       <input type="button" value="Login as Administrator">
