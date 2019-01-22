a04994a0c7873b6a5858797f075fdb088b3a8540
nobleOn3 <nob16001@byui.edu>
2019-01-22 10:33:29 -0700

    adding practice form stuff

---
+++ web/formAct/results.php
@@ -0,0 +1,11 @@
+<?php
+   $name = $_GET['Name'];
+   $email= $_GET['Email'];
+   $major= $_GET['major'];
+   $comments= $_GET['comment'];
+
+   echo "Your name: $name";
+   echo "Your email: $email";
+   echo "Your major: $major";
+   echo "Comment: $comments";
+   ?>
