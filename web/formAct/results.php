818066562554e1e47fab902de713e4ec95817470
nobleOn3 <nob16001@byui.edu>
2019-01-22 10:47:56 -0700

    adding practice form stuff

--- web/formAct/results.php
+++ web/formAct/results.php
@@ -8,4 +8,29 @@
    echo "Your email: $email<br/>";
    echo "Your major: $major<br/>";
    echo "Comment: $comments<br/>";
+   displayPlaces();
+
+   function displayPlaces {
+   	if (isset($_GET['nA'])) {
+   		echo "You have been to North America."
+   	}
+   	if (isset($_GET['sA'])) {
+   		echo "You have been to South America."
+   	}
+   	if (isset($_GET['eU'])) {
+   		echo "You have been to Europe."
+   	}
+   	if (isset($_GET['asia'])) {
+   		echo "You have been to Asia."
+   	}
+   	if (isset($_GET['aF'])) {
+   		echo "You have been to Africa."
+   	}
+   	if (isset($_GET['ant'])) {
+   		echo "You have been to Antartica."
+   	}
+   	if (isset($_GET['aU'])) {
+   		echo "You have been to Australia."
+   	}
+   }
    ?>
