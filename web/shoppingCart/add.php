6b26cfe88a29e5cdfe774c77cd9cba60687df3c0
nobleOn3 <nob16001@byui.edu>
2019-01-26 15:50:45 -0700

    still experimenting with add.php

--- web/shoppingCart/add.php
+++ web/shoppingCart/add.php
@@ -1,7 +1,7 @@
 <?php 
    session_start();
 
-   array_merge($_SESSION['cart'], array("mDice"=>"10");
+   array_merge($_SESSION['cart'], array("mDice"=>"10"));
 
    print_r($_SESSION['cart']);
 ?>
