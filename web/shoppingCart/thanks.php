6970df9059587de26ba3253c347cc0038891ce1e
nobleOn3 <nob16001@byui.edu>
2019-01-26 21:00:21 -0700

    fixing a bug in the thank you page

--- web/shoppingCart/thanks.php
+++ web/shoppingCart/thanks.php
@@ -66,9 +66,9 @@ function displayAddress() {
 <body>
     <div class="main">
    	  <div class="center">
-        <h1>Thank you <?php displayName(); ?>!</h1>
+        <h1>Thank you</h1>
         <?php displayName(); ?>
-        <h2>Your order will be sent to: <?php displayAddress(); ?></h2>
+        <h2>Your order will be sent to:</h2>
         <h3>Your order: </h3>
         <?php makeTable(); ?>
       </div>
