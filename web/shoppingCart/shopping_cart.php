5220dee742a6c28de937448074d7d8a200fa2149
nobleOn3 <nob16001@byui.edu>
2019-01-26 17:32:56 -0700

    changing some spacing in the cart page

--- web/shoppingCart/shopping_cart.php
+++ web/shoppingCart/shopping_cart.php
@@ -6,7 +6,7 @@ function display_metal_dice() {
    if($_SESSION['cart']['mDice'] > 0) {
       	echo "<div><div><h3>Metal Dice: <span>";
       	echo $_SESSION['cart']['mDice'];
-      	echo "<span></div><br/><div><form action='remove.php' method='post'><input type='submit' value='Remove'></form></div></div><br/>";
+      	echo "<span></div><div><form action='remove.php' method='post'><input type='submit' value='Remove'></form></div></div><br/>";
        } 
 }
 
@@ -14,7 +14,7 @@ function display_plastic_dice() {
     if($_SESSION['cart']['pDice'] > 0) {
       	echo "<div><div><h3>Plastic Dice: <span>";
       	echo $_SESSION['cart']['pDice'];
-      	echo "<span></div><br/><div><form action='removepDice.php' method='post'><input type='submit' value='Remove'></form></div></div><br/>";
+      	echo "<span></div><div><form action='removepDice.php' method='post'><input type='submit' value='Remove'></form></div></div><br/>";
     }
 }
 
@@ -22,7 +22,7 @@ function display_dungeon_master() {
 	if($_SESSION['cart']['dmBk'] > 0) {
       	echo "<div><div><h3>Dungeon Master Handbook(s): <span>";
       	echo $_SESSION['cart']['dmBk'];
-      	echo "<span></div><br/><div><form action='removeDM.php' method='post'><input type='submit' value='Remove'></form></div></div><br/>";
+      	echo "<span></div><div><form action='removeDM.php' method='post'><input type='submit' value='Remove'></form></div></div><br/>";
        } 
 }
 
@@ -30,7 +30,7 @@ function display_monster_manual() {
 	if($_SESSION['cart']['monMnl'] > 0) {
       	echo "<div><div><h3>Monster Manual(s): <span>";
       	echo $_SESSION['cart']['monMnl'];
-      	echo "<span></div><br/><div><form action='removeMonMnl.php' method='post'><input type='submit' value='Remove'></form></div></div><br/>";
+      	echo "<span></div><div><form action='removeMonMnl.php' method='post'><input type='submit' value='Remove'></form></div></div><br/>";
        } 
 }
 
