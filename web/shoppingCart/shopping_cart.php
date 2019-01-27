9f37edb11ab3abfcafe7bd4d62b34881bdd3b4bd
nobleOn3 <nob16001@byui.edu>
2019-01-26 17:11:51 -0700

    trying to fix an error with the cart

--- web/shoppingCart/shopping_cart.php
+++ web/shoppingCart/shopping_cart.php
@@ -4,7 +4,9 @@ session_start();
 
 function display_metal_dice() {
    if($_SESSION['cart']['mDice'] > 0) {
-      	echo "<div><div><h3>Metal Dice:<span>$_SESSION['cart']['mDice']<span></div><br/><div><form action='remove.php' method='post'><input type='submit' value='Remove'></form></div></div><br/>";
+      	echo "<div><div><h3>Metal Dice:<span>";
+      	echo $_SESSION['cart']['mDice'];
+      	echo "<span></div><br/><div><form action='remove.php' method='post'><input type='submit' value='Remove'></form></div></div><br/>";
        } 
 }
 
