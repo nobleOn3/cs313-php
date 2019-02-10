65c924575e4798f951632b22f5763e9eb5f5a8a4
nobleOn3 <nob16001@byui.edu>
2019-02-09 18:41:33 -0700

    Actually added a page to display the thank you images.

---
+++ web/thanks/display_pic.php
@@ -0,0 +1,33 @@
+<?php
+/***********************************************
+* This page shows the list of thank you images
+* and provides links to see them.
+***********************************************/
+
+require "dbConnect.php";
+$db = get_db();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Display Thank You Message</title>
+	<link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+	<div>
+
+		<?php
+
+		    $name = $_GET['i_name'];
+		    $file = $_GET['i_file'];
+
+		    echo "<h1>$name</h1>";
+
+		   	echo "<img src='$file' alt = '$name'/>";
+
+		?>
+
+	</div>
+</body>
+</html>
