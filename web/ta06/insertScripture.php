5e4fd097ed3907b9a145f2d595d0ad922b8c993f
nobleOn3 <nob16001@byui.edu>
2019-02-12 10:58:59 -0700

    Added some stuff for the team activity

--- web/ta06/insertScripture.php
+++ web/ta06/insertScripture.php
@@ -13,9 +13,10 @@
    $statement->execute();
 
    $new_id = $pdo->lastInsertId('scriptures_id_seq');
+   echo $new_id;
 
-   $statement = $db->prepare("INSERT INTO script_topics(script_id, topic_id) VALUES ($new_id, $topic);");
-   $statement->execute();
+   //$statement = $db->prepare("INSERT INTO script_topics(script_id, topic_id) VALUES ($new_id, $topic);");
+   //$statement->execute();
 
 
 
