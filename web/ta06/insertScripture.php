e854bc744cf384b1b1a738263565d5fa4c84391b
nobleOn3 <nob16001@byui.edu>
2019-02-12 10:41:48 -0700

    Added some stuff for the team activity

--- web/ta06/insertScripture.php
+++ web/ta06/insertScripture.php
@@ -9,7 +9,7 @@
    $content = $_POST['content'];
    $topic = $_POST['topic'];
 
-   $statement = $db->prepare("INSERT INTO scripture(book, chapter, verse, content) VALUES ('$book', $chap, verse, 'content');");
+   $statement = $db->prepare("INSERT INTO scriptures(book, chapter, verse, content) VALUES ('$book', $chap, verse, 'content');");
    $statement->execute();
 
 
