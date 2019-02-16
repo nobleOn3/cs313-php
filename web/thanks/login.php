51416bd23e82bd64420074d387516ab686cb9b64
nobleOn3 <nob16001@byui.edu>
2019-02-15 22:10:53 -0700

    Added login.php

---
+++ web/thanks/login.php
@@ -0,0 +1,29 @@
+<?php
+/***********************************************
+* This page allows a user to create an 
+* "account".
+***********************************************/
+
+require "dbConnect.php";
+$db = get_db();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="thanks.css">
+</head>
+<body>
+	<div class="center">
+		<h1 class="titleLime">Login</h1>
+
+        <form action="addNewUser.php" method="POST">
+        	Username: <input type="text" name="username"/>
+        	Password: <input type="text" name="pass"/>
+        	<input type="submit" value="Sign Up!" />
+        </form>
+
+	</div>
+</body>
+</html>
