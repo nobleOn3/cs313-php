4ae465f6497cfca67f6b01add7de3f51090c4811
nobleOn3 <nob16001@byui.edu>
2019-02-16 15:23:05 -0700

    fixing a syntax error in add_new_user.php)

--- web/thanks/add_new_user.php
+++ web/thanks/add_new_user.php
@@ -11,13 +11,13 @@ $user = htmlspecialchars($_POST['username']);
 $pass = htmlspecialchars($_POST['pass']);
 $name = htmlspecialchars($_POST['name']);
 
-$stmt = $db->prepare('INSERT INTO user(name, pass, user_real) VALUES (:user, :pass, :user_r);');
+$stmt = $db->prepare('INSERT INTO users(name, pass, user_real) VALUES (:user, :pass, :user_r);');
 $stmt->bindValue(':user', $user, PDO::PARAM_STR);
 $stmt->bindValue(':pass', $pass, PDO::PARAM_STR);
 $stmt->bindValue(':user_r', $name, PDO::PARAM_STR);
 $stmt->execute();
 
-$new_page="thank_you_list.php?user=$user";
+$new_page = "thank_you_list.php?user=$user";
 header("Location: $new_page");
 die();
 
