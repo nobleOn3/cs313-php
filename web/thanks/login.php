5972799fa175ca307917699f651352f7c0261dd3
nobleOn3 <nob16001@byui.edu>
2019-02-15 22:21:33 -0700

    changed some formatting of login.php

--- web/thanks/login.php
+++ web/thanks/login.php
@@ -19,8 +19,8 @@ $db = get_db();
 		<h1 class="titleLime">Login</h1>
 
         <form action="addNewUser.php" method="POST">
-        	Username: <input type="text" name="username"/><br/>
-        	Password: <input type="text" name="pass"/><br/>
+        	Username: <input type="text" name="username"/><br/><br/>
+        	Password: <input type="text" name="pass"/><br/><br/>
         	<input type="submit" value="Sign Up!" />
         </form>
 
