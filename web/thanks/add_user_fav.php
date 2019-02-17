a0dab9d76679d2514ae136e90a21f1d4d3980d30
nobleOn3 <nob16001@byui.edu>
2019-02-16 21:37:20 -0700

    trying to get add_user_fav.php to work...

--- web/thanks/add_user_fav.php
+++ web/thanks/add_user_fav.php
@@ -10,23 +10,26 @@ $db = get_db();
 $i_file = $_GET['i_file'];
 $user = $_GET['user'];
 
-$query = "SELECT id FROM users WHERE user = '$user'";
-$stmt = $db->prepare($query);
-$stmt->execute();
-$user_array = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$user_id = $user_array['id'];
+echo "Si_file";
+echo "$user";
 
-$query = "SELECT id FROM thank_you_images WHERE image_file = '$i_file'";
-$stmt = $db->prepare($query);
-$stmt->execute();
-$img_array = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$img_id = $img_array['id'];
+// $query = "SELECT id FROM users WHERE user = '$user'";
+// $stmt = $db->prepare($query);
+// $stmt->execute();
+// $user_array = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// $user_id = $user_array['id'];
 
-$query = "INSERT INTO user_favorites(user_id, image_id) VALUES ($user_id, $img_id)";
-$stmt = $db->prepare($query);
-$stmt->execute();
+// $query = "SELECT id FROM thank_you_images WHERE image_file = '$i_file'";
+// $stmt = $db->prepare($query);
+// $stmt->execute();
+// $img_array = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// $img_id = $img_array['id'];
 
-header("Location: thank_you_list.php");
-die();
+// $query = "INSERT INTO user_favorites(user_id, image_id) VALUES ($user_id, $img_id)";
+// $stmt = $db->prepare($query);
+// $stmt->execute();
+
+// header("Location: thank_you_list.php");
+// die();
 
 ?>
