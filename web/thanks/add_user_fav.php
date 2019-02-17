9a38944b2b55771964f5402e2f37927a9a9e0245
nobleOn3 <nob16001@byui.edu>
2019-02-16 17:11:01 -0700

    trying to get add_user_fav.php to work...

--- web/thanks/add_user_fav.php
+++ web/thanks/add_user_fav.php
@@ -10,12 +10,12 @@ $db = get_db();
 $i_file = $_GET['i_file'];
 $user = $_GET['user'];
 
-$query = "SELECT id FROM users WHERE user = $user";
+$query = "SELECT id FROM users WHERE user = '$user'";
 $stmt = $db->prepare($query);
 $stmt->execute();
 $user_id = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$query = "SELECT id FROM thank_you_images WHERE image_file = $i_file";
+$query = "SELECT id FROM thank_you_images WHERE image_file = '$i_file'";
 $stmt = $db->prepare($query);
 $stmt->execute();
 $img_id = $stmt->fetchAll(PDO::FETCH_ASSOC);
