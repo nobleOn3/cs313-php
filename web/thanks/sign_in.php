1ef469c64faa74ee15afa73638e63696b36c3943
nobleOn3 <nob16001@byui.edu>
2019-02-25 21:38:36 -0700

    added the needed page to properly log in a current user

---
+++ web/thanks/sign_in.php
@@ -0,0 +1,31 @@
+<?php
+/***********************************************
+* This page allows a user to create an 
+* "account".
+***********************************************/
+
+require "dbConnect.php";
+$db = get_db();
+
+$user = htmlspecialchars($_POST['username']);
+$pass = htmlspecialchars($_POST['pass']);
+
+$stmt = $db->prepare("SELECT pass FROM users WHERE user = $user");
+$stmt->execute();
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $pass_real = $row['name'];
+}
+
+if(password_verify("$pass", "$pass_real")) {
+   $new_page = "thank_you_list.php?user=$user";
+   header("Location: $new_page");
+   die();
+}
+
+else {
+	$new_page = "login_current_user.php";
+    header("Location: $new_page");
+    die();
+}
+
+?>
